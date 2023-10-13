db1b5beca76ee6d6c35915172c46c4b5278fde01
mauriciobrambila <mauriciobrambila2015@gmail.com>
2023-10-13 18:54:18 -0300

    feat:  primeiro commit

---
+++ Persistencia/Conexao.js
@@ -0,0 +1,29 @@
+import mysql from "mysql2/promise";
+
+export default async function conectar() {
+  if (global.conexao && global.conexao.status != "disconnected") {
+    return global.conexao;
+  }
+
+  const conexao = await mysql.createConnection({
+    host: "localhost",
+    user: "aluno45-pfsii",
+    porta: 3306,
+    password: "y5nlbzEzW9VUYXh3IG2D",
+    database: "motoboys",
+   });
+
+   //LOCAL
+  //const conexao = await mysql.createConnection({
+       // host: "localhost",
+       // user: "root",
+       // password: "aluno45-pfsii",
+       // database: "motoboys"
+    //  });
+    
+      global.conexao = conexao;
+      return conexao;
+    }
+
+
+    
---
+++ index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import rotaMotoboy from "./Rotas/rotaMotoboy.js";
+import rotaEntrega from "./Rotas/rotaEntrega.js";
+import rotaPedido from "./Rotas/rotaPedido.js";
+
+const app = express();
+
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+app.use(cors());
+app.use('/motoboys', rotaMotoboy);
+app.use('/entrega', rotaEntrega);
+app.use('/pedidos', rotaPedido);
+
+//app.listen(3000, "localhost", ()=>{
+    //console.log("Rodando em http://localhost:3000/motoboys")
+//});
+
+app.listen(4045, '0.0.0.0', ()=>{
+    console.log("Backend ouvindo em http://localhost:4045")
+});
